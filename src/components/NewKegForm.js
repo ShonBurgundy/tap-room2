db518c0908485171550d32c1589c6162ecd86d64
shonburgundy <seanbeloussow@yahoo.com>
2020-07-02 20:11:21 -0700

    update newkegform with inputs and button

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { v4 } from 'uuid';
 
 function NewKegForm(props){
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+          <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Beer Name' />
+        <input
+          type='text'
+          name='brand'
+          placeholder='Brand' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' />
+        <input
+          type='text'
+          name='abv'
+          placeholder='ABV' />  
+        <button type='submit'>Add Keg</button>
+      </form>
     </React.Fragment>
   );
 }
