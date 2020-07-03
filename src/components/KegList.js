d283ba1f18eae66ef2e0d9e6c0b265114104b795
shonburgundy <seanbeloussow@yahoo.com>
2020-07-02 17:36:51 -0700

    add app diagram to README

---
+++ src/components/KegList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Tap Room</h1>
+      <h3>BatSquatch</h3>
+      <h3>Hazy IPA</h3>
+      <h3>8.0% ABV</h3>
+      <h3>124 Pints</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
