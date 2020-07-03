115986b64dec947302e0605c6612871ed889ad87
shonburgundy <seanbeloussow@yahoo.com>
2020-07-02 18:40:32 -0700

    add loop to keglist component

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -2,15 +2,44 @@ import React from 'react';
 import Keg from './Keg';
 // import PropTypes from 'prop-types';
 
+const masterKegList = [
+  {
+    name: 'Thato and Haley',
+    brand: '3A',
+    price: 'Firebase won\'t save record. Halp.',
+    abv: '8',
+    fullKeg: 124
+  },
+  {
+    name: 'Sleater and Kinney',
+    brand: '4B',
+    price: 'Prop types are throwing an error.',
+    abv: '8',
+    fullKeg: 124
+  },
+  {
+    name: 'Imani & Jacob',
+    brand: '9F',
+    price: 'Child component isn\'t rendering.',
+    abv: '8',
+    fullKeg: 124
+  }
+];
 
 function KegList() {
   return (
-    <Keg 
-      name = "BatSquatch"
-      brand = "Rogue" 
-      price = "$6.00/pint"
-      abv = "8.0% ABV"
-      fullKeg = {124} />
+    <React.Fragment>
+      <hr/>
+      {masterKegList.map((keg, index) =>
+        <Keg 
+          name = {keg.name}
+          brand = {keg.brand} 
+          price = {keg.price}
+          abv = {keg.abv}
+          fullKeg = {124}
+          key = {index} />
+        )}
+    </React.Fragment>
   );
 }
 
