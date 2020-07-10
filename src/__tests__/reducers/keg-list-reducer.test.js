ea5b37a0c31783366f35dfd655b85c167a19d7e5
shonburgundy <seanbeloussow@yahoo.com>
2020-07-10 10:55:41 -0700

    add failing test for ADD_TICKET reducer

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -15,4 +15,27 @@ describe('kegListReducer', () =>  {
   test('Should return default state if there is no action type passed into reducer', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
+
+  test('Should successfully add new keg to masterKegList', () => {
+    const {name, brand, price, abv, quantity, id} = ticketData;
+    action = {
+      type: 'ADD_TICKET',
+      name: name,
+      brand: brand,
+      price: price,
+      abv: abv,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        abv: abv,
+        id: id
+      }
+    });
+  });
+
 });
