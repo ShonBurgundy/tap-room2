45747b284314bfc15489fb5a7138110f8eb0f785
shonburgundy <seanbeloussow@yahoo.com>
2020-07-10 10:44:09 -0700

    add kegData const to reducer tests

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,6 +1,17 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () =>  {
+
+  let action;
+  const kegData = {
+    name: 'Batsquatch',
+    brand: 'Rogue',
+    price: '6',
+    abv: '8',
+    quantity: '124',
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into reducer', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
