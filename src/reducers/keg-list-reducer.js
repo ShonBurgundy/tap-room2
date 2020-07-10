d033c30adf3a09139102dbd644bb46b207f36b7d
shonburgundy <seanbeloussow@yahoo.com>
2020-07-10 11:52:31 -0700

    create REDUX store

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,5 +1,5 @@
 export default (state = {}, action) => {
-  const { name, brand, price, abv, id } = action;
+  const { name, brand, price, abv, quantity, id } = action;
   switch(action.type) {
   case 'ADD_KEG':
     return Object.assign({}, state, {
@@ -8,6 +8,7 @@ export default (state = {}, action) => {
         brand: brand,
         price: price,
         abv: abv,
+        quantity: quantity,
         id: id
       }
     });
