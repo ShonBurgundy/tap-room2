939d552605441dd0bb3f2eeebb58edc297ee6862
shonburgundy <seanbeloussow@yahoo.com>
2020-07-10 16:37:38 -0700

    pass actions index test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -2,7 +2,7 @@ import * as actions from './../../actions';
 
 describe('tap room actions', () => {
   it('deleteKeg should create DELETE_KEG action', () => {
-    expect(actions.deleteTicket(1)).toEqual({
+    expect(actions.deleteKeg(1)).toEqual({
       type: 'DELETE_KEG',
       id: 1
     });
