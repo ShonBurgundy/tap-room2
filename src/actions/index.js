d02b0f543534239a930f08878f40f3f548312ada
shonburgundy <seanbeloussow@yahoo.com>
2020-07-10 16:48:11 -0700

    passing ADD_KEG action test

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,15 @@ export const deleteKeg = id => ({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const addKeg = (keg) => {
+  const { name, brand, price, abv, id} = keg;
+  return {
+    type: 'ADD_KEG',
+    name: name,
+    brand: brand,
+    price: price,
+    abv: abv,
+    id: id
+  }
+}
